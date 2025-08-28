018a04bd6d6af9ca31ff882c4e8d47f303054636
Ben-Zama <benzamasimon@gmail.com>
2025-08-28 01:45:15 +0100

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="main">
+
+    <!-- Header -->
+    <header></header>
+
+    <!-- Hero section -->
+    <section class="hero"></section>
+
+    <!-- Overview section -->
+    <section class="overview"></section>
+
+    <!-- Room details -->
+    <section class="room_details"></section>
+
+    <!-- Gallery -->
+    <section class="gallery"></section>
+
+    <!-- Floorplans -->
+    <section class="floorplans"></section>
+
+    <!-- Highlights -->
+    <section class="highlights"></section>
+
+    <!-- Conact -->
+    <section class="contact"></section>
+
+    <!-- Footer -->
+    <footer></footer>
+
+  </div>
+</template>
+
+<script setup>
+
+
+</script>
